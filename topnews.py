31e2ade9ba1ec9d159215b5cdb36c85dac7e76e8
Sidd <chaberiasiddharth3@gmail.com>
2024-08-30 17:50:12 +0530

    Pushed the file to the live repo

---
+++ topnews.py
@@ -0,0 +1,22 @@
+import time
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from plyer import notification
+
+url = "https://news.google.com/news/rss"
+
+xml_data = urlopen(url).read()
+urlopen(url).close()
+
+sp = BeautifulSoup(xml_data, "xml")
+news_list = sp.find_all('item')
+news_list = news_list[0:11] 
+
+for news in news_list:
+	notification.notify(
+		title="Top Headlines of Today",
+		message = news.title.text + "\n Published on: "+ news.pubDate.text,
+		timeout = 2)
+
+	time.sleep(10)
+
