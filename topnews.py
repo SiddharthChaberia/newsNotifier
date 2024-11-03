347295c49d57e7244440e30bb90a68b4578c9822
Siddharth Chaberia <72343060+SiddharthChaberia@users.noreply.github.com>
2024-11-03 21:48:57 +0530

    Update topnews.py

--- topnews.py
+++ topnews.py
@@ -4,19 +4,23 @@ from urllib.request import urlopen
 from plyer import notification
 
 url = "https://news.google.com/news/rss"
+while True:
+	try:
+		xml_data = urlopen(url).read()
+		urlopen(url).close()
 
-xml_data = urlopen(url).read()
-urlopen(url).close()
+		sp = BeautifulSoup(xml_data, "xml")
+		news_list = sp.find_all('item')
+		news_list = news_list[0:21] 
 
-sp = BeautifulSoup(xml_data, "xml")
-news_list = sp.find_all('item')
-news_list = news_list[0:11] 
+		for news in news_list:
+			notification.notify(
+				title="Top Headlines of Today",
+				message = news.title.text + "\n Published on: "+ news.pubDate.text,
+				timeout = 4)
 
-for news in news_list:
-	notification.notify(
-		title="Top Headlines of Today",
-		message = news.title.text + "\n Published on: "+ news.pubDate.text,
-		timeout = 2)
-
-	time.sleep(10)
-
+			time.sleep(20)
+	except Exception as e:
+		print("Some exception occured:", e)
+	
+	time.sleep(1800)
